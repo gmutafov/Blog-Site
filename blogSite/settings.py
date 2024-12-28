095d6eac6839042b7dd7742f3eddea92219d747d
Georgi Mutafov <145926316+gmutafov@users.noreply.github.com>
2024-12-28 22:13:58 +0200

    Minor changes

--- blogSite/settings.py
+++ blogSite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,14 +81,8 @@ WSGI_APPLICATION = 'blogSite.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config('DB_NAME'),
-        "USER": config('DB_USER'),
-        "PASSWORD": config('DB_PASS'),
-        "HOST": config('DB_HOST'),
-        "PORT": config('DB_PORT'),
-    }
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
+
 }
 
 
